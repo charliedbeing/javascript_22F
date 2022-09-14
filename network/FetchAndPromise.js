6b1697bd7fd2ac6ac574131d3c08ef525fd8d7fd
charliedbeing <zding12@my.centennialcollege.ca>
2022-09-14 09:36:02 -0400

    wangyi js

--- network/FetchAndPromise.js
+++ network/FetchAndPromise.js
@@ -1,1 +1,36 @@
-console.log('fetch');
+// example 01
+const cl= (m)=> console.log(m)
+
+const promise = new Promise((resolve,rejcet)=>{
+    resolve('I have finished');
+}).then(data=>{
+    console.log(data);
+    return "Charlie";
+}).then(data=>{
+    console.log(data)
+})
+
+//example 02 
+
+const promise2 = new Promise((resolve, reject)=>{
+    reject('error');
+}).catch(e=>{
+    console.log(e)
+})
+
+
+const test1 = new Promise((resolve,rejcet)=>{
+    resolve('I have finished');
+}).then(data=>{
+    console.log(data);
+    return "Charlie";
+})
+
+cl(test1)
+
+test1.then(data=>{
+    console.log(data);
+})
+
+
+
